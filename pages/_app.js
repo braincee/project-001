104ff4376a4b86c10ea496c4d61c5df80ee5b59d
Achilles-Dev <solohag92@gmail.com>
2023-05-18 10:47:39 +0000

    embedding-videos

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,7 @@
+import React from "react";
 import { NextUIProvider } from '@nextui-org/react';
+
+import '@/styles/globals.css';
 
 export default function App({ Component, pageProps }) {
   return (
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,40 @@
 import Head from 'next/head';
-
+import { Input, Button } from '@nextui-org/react';
+import { useState } from 'react';
 
 export default function Home() {
+  const [urlId, setUrlId] = useState('');
+  const [urlIds, setUrlIds] = useState([]);
+  const [isvalidated, setIsvalidated] = useState(true);
+
+  const isValidHttpUrl = (string) => { 
+    try {
+      const newUrl = new URL(string);
+      console.log(newUrl);
+      return newUrl.protocol === 'https:' && newUrl.host === 'www.youtube.com' ;
+    } catch (err) {
+      return false;
+    }
+  }
+
+  const handleUrlInput = (e) => {   
+    if (isValidHttpUrl(e.target.value) === true) {
+      setUrlId(e.target.value);  
+      setIsvalidated(true); 
+    } else {
+      setIsvalidated(false);
+    }
+  }
+
+  console.log(urlId);
+  const updateurlIds = () => {
+    if (urlId !== '' && isvalidated) {
+      let video_id = urlId.split('v=')[1];   
+      setUrlIds([...urlIds, video_id]);
+    }
+    setUrlId('');
+  }
+
   return (
     <>
       <Head>
@@ -10,8 +43,30 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
-        
+      <main className="mt-4 mb-[50px] flex flex-col">
+        <h1 className="text-center text-[30px]">YT Playlist Creator and Sharer</h1>
+        <div className="flex justify-center items-center mt-[50px] gap-4 min-h-auto">
+          <Input  
+            onChange={handleUrlInput}
+            className="w-3/5 !placeholder:text-slate-400 placeholder:text-[20px]" 
+            placeholder="Add YouTube Url"
+            aria-labelledby="none"
+            value={urlId}
+          />
+          <Button className="w-1/5 bg-sky-500/100 text-white" size="xl" onPress={updateurlIds}>Add URL</Button>
+        </div>
+        { isvalidated === false ? (
+          <span className="text-danger text-[24px] px-[120px]">Invalid URL</span>
+        ) : ""}
+        <div className="mt-[20px] px-[120px] flex flex-col gap-[20px]">
+          { urlIds.length > 0 ? urlIds.map((url) => (
+            <div key={url}>
+              <div className="video">
+                  <iframe width="100%" height="100%" src={`https://youtube.com/embed/${url}`} title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+              </div>
+            </div>
+          )): ''}
+        </div>
       </main>
     </>
   )
