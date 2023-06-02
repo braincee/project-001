f7bd208e3975603d562495aad6695f9ba866267b
braincee <kwesi938@gmail.com>
2023-06-02 08:32:24 +0000

    Add iframe videos to search results component

--- pages/components/SearchResults.js
+++ pages/components/SearchResults.js
@@ -1,18 +1,30 @@
 import React from 'react';
-import { Image } from '@nextui-org/react';
 
 const SearchResults = ({ videos }) => {
   return (
-    <div className="mt-[20px] px-[20px] lg:px-[100px] flex flex-col gap-[20px]">
-      {videos.length > 0 &&
-        videos.map((video, index) => (
-          <div key={index}>
-            <h3>{video.title}</h3>
-            <p>{video.description}</p>
-            <Image src={`https://i.ytimg.com/vi/${video.id}/default.jpg`} alt="Video Thumbnail" width={320} height={180} />
+    <div className="grid gap-4">
+      {videos.map((video) => (
+        <div key={video.id}>
+          <h6 className="text-lg font-bold">{video.title}</h6>
+          <div className="relative aspect-w-16 aspect-h-9">
+          <div className="video">
+            <iframe width="100%" 
+                    height="100%" 
+              src={`https://youtube.com/embed/${video.id}`} 
+              title="YouTube video player" 
+              allow="accelerometer; autoplay; 
+              clipboard-write; 
+              encrypted-media; 
+              gyroscope; picture-in-picture" 
+              allowFullScreen>
+            </iframe>
           </div>
-        ))}
+          <p className="mt-2">{video.description}</p>
+        </div>
+        </div>
+      ))}
     </div>
+
   );
 };
 
