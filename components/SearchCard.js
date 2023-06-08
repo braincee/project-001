2b4195139ffa1ef44b75830562b11994f46d8bea
braincee <kwesi938@gmail.com>
2023-06-08 00:12:55 +0000

    Update search card display

--- components/SearchCard.js
+++ components/SearchCard.js
@@ -6,11 +6,11 @@ const SearchCard = ({ video, addToList }) => {
 
   return (
     <div className="search-video max-w-[300px]">
-      <Card className="video-link" isPressable onPress={() => addToList(video.id, video.title, video.description)} >
-        <div className="search-card flex flex-col gap-4 py-4 bg-white rounded-lg shadow-md">
-          <Image className="rounded-b-none w-[300px]" src={`http://img.youtube.com/vi/${video.id}/sddefault.jpg`} alt="Youtube Video"/>
+      <Card className="video-link hover:scale-95" isPressable onPress={() => addToList(video.id, video.title, video.description)} >
+        <div className="search-card flex flex-col gap-4 py-4 bg-black rounded-lg shadow-md">
+          <Image className="rounded-b-none bg-blue-400 w-[300px]" src={`http://img.youtube.com/vi/${video.id}/sddefault.jpg`} alt="Youtube Video"/>
           <div className="w-full px-3">
-            <p className="text-lg font-bold">{video.title}</p>
+            <p className="text-sm text-gray-200">{video.title}</p>
           </div>
         </div>
       </Card>
