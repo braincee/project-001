6a727a3505039bd4d5da3e1d090c02a9e05721c2
Achilles-Dev <solohag92@gmail.com>
2023-06-17 16:29:58 +0000

    Generate sitemaps

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,5 +2,10 @@
 module.exports = {
     siteUrl: 'http://localhost:3000/',
     generateRobotsTxt: true, // (optional)
-    // ...other options
+    robotsTxtOptions: {
+      additionalSitemaps: [
+        'http://localhost:3000/sitemap.xml',
+        'http://localhost:3000/sitemap-0.xml',
+      ]
+    }
   }
