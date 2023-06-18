6d4196a8040ace313e105ce251d1b675baff5dce
braincee <kwesi938@gmail.com>
2023-06-18 06:33:39 +0000

    ||

--- components/SkeletonBuilder.js
+++ components/SkeletonBuilder.js
@@ -6,7 +6,7 @@ const SkeletonBuilder = ({ cards }) => {
     <div className="flex md:flex-wrap md:flex-row flex-col mt-8 justify-center px-10 md:px-5 gap-4">
       { Array(cards).fill(0).map((item, i) => (
         <>
-        <div className="md:max-w-[230px] md:min-h-[280px] min-h-[320px]" key={i}>
+        <div className="md:max-w-[231px] md:min-h-[280px] min-h-[320px]" key={i}>
           <Card className="hover:scale-95 h-full" disabled>
             <div className="flex flex-col gap-4 bg-black rounded-lg shadow-md h-full">
               <div className="md:w-[300px] md:min-h-[220px] min-h-[360px] bg-gray-300 animate-pulse"></div>         
