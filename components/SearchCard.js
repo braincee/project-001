e906a49fa8d2379b6330fa56ae602dab2bab3496
Achilles-Dev <solohag92@gmail.com>
2023-06-23 11:58:22 +0000

    Update SearchCard component

--- components/SearchCard.js
+++ components/SearchCard.js
@@ -17,16 +17,16 @@ const SearchCard = ({ video, addToList, truncate }) => {
           <Image className="rounded-b-none bg-blue-400 w-full md:w-[300px] min-h-[360px] md:min-h-full" src={`http://img.youtube.com/vi/${video.id}/sddefault.jpg`} alt="Youtube Video"/>
           <div className="flex flex-row w-full p-3">
             <div>
-            <Avatar className="me-[8px] min-w-[30px] h-[30px]" src={`http://img.youtube.com/vi/${video.id}/sddefault.jpg`}/>
+            <Avatar className="me-[8px] min-w-[36px] h-[36px] border" src={`http://img.youtube.com/vi/${video.id}/sddefault.jpg`}/>
             </div>
           <div>
           <ul>
-          <li className="md:text-sm text-xl text-left text-gray-200">{decodeHTML(truncate(video.title, 15)) }</li>
+            <li className="md:text-sm text-xl text-left text-gray-200">{decodeHTML(truncate(video.title, 20)) }</li>
             <li className="flex items-center text-gray-500 text-[14px]">
-                {video.channelTitle}
-                <span className="ps-1"><FaCheckCircle className="min-w-[14px] h-[14px]" /></span>
-              </li>
-              <li className='text-gray-200 md:text-sm'>0 views. 1 hour ago</li>
+              {truncate(video.channelTitle, 15)}
+              <span className="ps-1"><FaCheckCircle className="min-w-[14px] h-[14px]" /></span>
+            </li>
+            <li className='text-gray-200 md:text-sm text-left'>0 views. 1 hour ago</li>
           </ul>
           </div>
           </div>
