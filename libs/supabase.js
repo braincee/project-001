29163b144321aab30c0a65ba01d82e363418f212
Achilles-Dev <solohag92@gmail.com>
2023-06-28 07:18:32 +0000

    Add utils file

--- libs/supabase.js
+++ libs/supabase.js
@@ -1,5 +1,7 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from "@supabase/supabase-js";
 
-const supabase = createClient('https://pztcvcpnqbocxaowaxmr.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB6dGN2Y3BucWJvY3hhb3dheG1yIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODc5MDMzOTgsImV4cCI6MjAwMzQ3OTM5OH0.AqcZgWXB1fIDxQCWdejroJeU89c-V-WhtRJRJmUVtqI')
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+const supabase = createClient(supabaseUrl, supabaseKey);
 
-export default supabase
+export default supabase;
